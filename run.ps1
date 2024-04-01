0c6dbd53b8caef29ad1d2066d4901ab058eeb131
Ron Nuriel <ron@nuriell.com>
2024-04-01 19:27:20 +0300

    run files fix

--- run.ps1
+++ run.ps1
@@ -1,13 +1,11 @@
-#!/bin/bash
-
 # Assuming this script is run from the Cycognito directory
 
 # Set input and output directories relative to the current script location
-input_dir="$(pwd)/input"
-output_dir="$(pwd)/output"
+$inputDir = "$(Get-Location)\input"
+$outputDir = "$(Get-Location)\output"
 
 # Build the Docker image
 docker build -t hometask/browser_module .
 
 # Run the Docker container, mounting the input and output directories
-docker run -v "$input_dir:/input" -v "$output_dir:/output" -it hometask/browser_module
+docker run -v "${inputDir}:/input" -v "${outputDir}:/output" -it hometask/browser_module
